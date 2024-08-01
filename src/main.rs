9e1baf8b647f8d0f3921ab3d7b87aa30e272f75a
Frank Lan <frank.lan@rea-group.com>
2024-08-01 18:31:08 +1000

    fixed last ip fetcher

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ mod database;
 use std::time::SystemTime;
 // mod kafka;
 use ip_fetcher::{fetch_ip_v4, fetch_ip_v6};
-use database::entities;
-use entities::{public_ips, prelude::PublicIps};
-use sea_orm::{ActiveValue, Database, DatabaseConnection, EntityTrait};
+use database::entities::{public_ips, prelude::PublicIps};
+use sea_orm::{ActiveValue, Database, DatabaseConnection, EntityTrait, QueryOrder};
 use tokio::join;
 use chrono::prelude::{DateTime, Utc};
 
@@ -21,14 +20,14 @@ async fn main() {
     return;
   }
   let new_public_ip = maybe_new_public_ip.unwrap();
-  
+
   on_ip_changed(&database, new_public_ip).await;
 }
 
 async fn is_ip_changed(database: &DatabaseConnection) -> Option<public_ips::ActiveModel> {
   let last_public_ip = get_last_public_ip(database).await;
   println!("Last public ip: {:?}", last_public_ip);
-  
+
   if last_public_ip.is_none()  {
     return None;
   }
@@ -36,12 +35,12 @@ async fn is_ip_changed(database: &DatabaseConnection) -> Option<public_ips::Acti
   let (ip_v4, ip_v6) = join!(fetch_ip_v4(), fetch_ip_v6());
   println!("public ipv4 address: {:?}", ip_v4);
   println!("public ipv6 address: {:?}", ip_v6);
-  
+
   let old = last_public_ip.unwrap();
   if old.ip_v4 == ip_v4 && old.ip_v6 == ip_v6 {
     return None
   }
-  
+
   Some(public_ips::ActiveModel {
     ip_v4: ActiveValue::Set(ip_v4),
     ip_v6: ActiveValue::Set(ip_v6),
@@ -57,7 +56,7 @@ async fn on_ip_changed(database: &DatabaseConnection, new_public_ip: public_ips:
 }
 
 async fn get_last_public_ip(db: &DatabaseConnection) -> Option<public_ips::Model> {
-  PublicIps::find().one(db).await.unwrap()
+  PublicIps::find().order_by_desc(public_ips::Column::CreatedAt).one(db).await.unwrap()
 }
 
 fn iso8601(st: std::time::SystemTime) -> String {
